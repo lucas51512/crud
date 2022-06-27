57535c6062b83e8c7bdd80bee11563e031da0fa6
lucas51512 <lucasbrancalhao454@gmail.com>
2022-06-27 11:53:05 -0300

    Interação com layout, validar os campos, cadastro no banco de dados localstorage, limpar os campos aposo cadastro feitos

--- pagina-principal/pagina-principal-js/main.js
+++ pagina-principal/pagina-principal-js/main.js
@@ -3,11 +3,13 @@
 const openModal = () => document.getElementById('modal')
     .classList.add('active');
 
-const closeModal = () => document.getElementById('modal')
-    .classList.remove('active');
+const closeModal = () => {
+    clearFields()
+    document.getElementById('modal').classList.remove('active')
+}
 
 const tempClient = {
-    nome: "Nicolas",
+    celular: "Nicolas",
     email: "Nicolas@gmail.com",
     celular: "18996258875",
     cidade: "Assis"
@@ -41,9 +43,65 @@ const createClient = (client) => {
     setLocalStorage(dbClient)
 }
 
+const isValidFields = () => {
+    return document.getElementById('form').reportValidity()
+}
+
+//Interação com o layout
+
+const clearFields = () => {
+    const fields = document.querySelectorAll('.modal-field')
+    fields.forEach(field => field.value = '')
+}
+
+//Interação com o Layout
+const saveClient = () => {
+    if(isValidFields()){
+        const client = {
+            nome: document.getElementById('nome').value,
+            email: document.getElementById('email').value,
+            celular: document.getElementById('celular').value,
+            cidade: document.getElementById('cidade').value
+        }
+        createClient(client);
+        clearFields();
+        closeModal();
+    }
+}
+
+const createRow = (client, index) => {
+    const newRow = document.createElement('tr')
+    newRow.innerHTML = `
+        <td>${client.nome}</td>
+        <td>${client.email}</td>
+        <td>${client.celular}</td>
+        <td>${client.cidade}</td>
+        <td>
+            <button type="button" class="button green" id="edit-${index}">Editar</button>
+            <button type="button" class="button red" id="delete-${index}" >Excluir</button>
+        </td>
+    `
+    document.querySelector('#tableClient>tbody ').appendChild(newRow)
+}
+
+const clearTable = () => {
+    const rows = document.querySelectorAll('#tableClient>tbody tr')
+    rows.forEach(row => row.parentNode.removeChild(row))
+}
+
+const updateTable = () => {
+    const dbClient = readClient();
+    clearTable();
+    dbClient.forEach(createRow);
+}
+
+updateTable();
+
 //Eventos 
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal);
 
 document.getElementById('modalClose')
     .addEventListener('click', closeModal);
+
+document.getElementById('salvar').addEventListener('click', saveClient);
